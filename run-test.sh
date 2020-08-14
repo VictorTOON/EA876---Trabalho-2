c8c606eaee15e3cc6be0953254dbcade0c6a926d
Victor Toon <araujo.victor200@gmail.com>
2020-08-14 16:13:53 -0300

    Saída sem MMap

--- run-test.sh
+++ run-test.sh
@@ -1,3 +1,3 @@
-for i in {1..20}; 
-	do (./build/linear.o 5 ./data/onepiece.jpg ; ./build/thread.o 5 ./data/onepiece.jpg; ./build/process.o 5 ./data/onepiece.jpg) | grep taken ;
+for i in {1..100};
+	do (./build/linear.o 5 ./data/cachorro.jpg ; ./build/thread.o 5 ./data/cachorro.jpg; ./build/process.o 5 ./data/cachorro.jpg) | grep taken ;
 done;
