8abbc0132e0315107505008c6fb25f31b348be3d
Victor Toon <araujo.victor200@gmail.com>
2020-08-14 15:16:08 -0300

    aaaa

--- run-test.sh
+++ run-test.sh
@@ -1,4 +1,3 @@
 for i in {1..20}; 
-	do (./build/linear.o 5 ./data/cachorro.jpg ; ./build/thread.o 5 ./data/cachorro.jpg; ./build/process.o 5 ./data/cachorro.jpg) | grep taken ; 
+	do (./build/linear.o 5 ./data/onepiece.jpg ; ./build/thread.o 5 ./data/onepiece.jpg; ./build/process.o 5 ./data/onepiece.jpg) | grep taken ;
 done;
-
